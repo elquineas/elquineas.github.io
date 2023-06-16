755efa3b951e0b5bb71c21f08dcba679aa4bfe75
elquineas <elquineas@naver.com>
2023-06-16 09:05:47 +0900

    김도연_수정

--- js/join.js
+++ js/join.js
@@ -1,19 +1,82 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const g_email = document.querySelector("#g_email");
-  //   g_email.addEventListener("focus", eamilfocus);
-  //   g_email.addEventListener("blur", eamilblur);
-  g_email.addEventListener("keyup", eamilKeyUp);
-  //   g_email.style.backgroundColor = "yellow";
-});
-// const eamilfocus = function () {
-//   g_email.style.border = "2px solid #ff1212";
-// };
-// const eamilblur = function () {
-//   g_email.style.border = "1px solid mediumseagreen";
-// };
+const INPUT_INDEX = {
+  EMAIL: 0,
+  PASSWORD: 1,
+  RE_PASSWORD: 2,
+  TEL: 3,
+  BIRTH: 4,
+};
 
-const eamilKeyUp = function () {
-  //   const g_email = document.querySelector("#g_email");
+document.addEventListener("DOMContentLoaded", () => {
+  const msg_divs = document.querySelectorAll("div.msg_div");
+
+  const join = () => {
+    const join_inputs = document.querySelectorAll(".contain input");
+    const input_email = join_inputs[INPUT_INDEX.EMAIL].value;
+    if (!input_email) {
+      const nav_text = "* 이메일을 입력해주세요";
+      msg_divs[INPUT_INDEX.EMAIL].style.display = "block";
+      msg_divs[INPUT_INDEX.EMAIL].textContent = nav_text;
+      msg_divs[INPUT_INDEX.EMAIL].style.color = "tomato";
+      return false;
+    }
+    const email_check =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (!email_check.test(input_email)) {
+      const nav_text = "* 이메일 형식을 맞춰서 입력해주세요";
+      msg_divs[INPUT_INDEX.EMAIL].style.display = "block";
+      msg_divs[INPUT_INDEX.EMAIL].textContent = nav_text;
+      msg_divs[INPUT_INDEX.EMAIL].style.color = "tomato";
+      return false;
+    }
+
+    const input_password = join_inputs[INPUT_INDEX.PASSWORD].value;
+    if (!input_password) {
+      alert("Password is required.");
+      join_inputs[INPUT_INDEX.PASSWORD].focus();
+      return false;
+    }
+    const password_check =
+      /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+    if (!password_check.test(input_password)) {
+      alert(
+        "Password must be 8 to 15 digits including \nspecial characters, uppercase and lowercase letters, and numbers."
+      );
+      join_inputs[INPUT_INDEX.PASSWORD].focus();
+      return false;
+    }
+
+    const input_re_password = join_inputs[INPUT_INDEX.RE_PASSWORD].value;
+    if (!input_re_password) {
+      alert("Password Check is required.");
+      join_inputs[INPUT_INDEX.RE_PASSWORD].focus();
+      return false;
+    }
+    if (input_password !== input_re_password) {
+      alert("Passwords do not match.");
+      join_inputs[INPUT_INDEX.RE_PASSWORD].value = "";
+      join_inputs[INPUT_INDEX.RE_PASSWORD].focus();
+      return false;
+    }
+
+    const input_tel = join_inputs[INPUT_INDEX.TEL].value;
+    if (!input_tel) {
+      alert("Phone is required.");
+      join_inputs[INPUT_INDEX.TEL].focus();
+      return false;
+    }
+    const tel_check = /^\d{3}-\d{3,4}-\d{4}$/;
+    if (!tel_check.test(input_tel)) {
+      alert(
+        "The phone number format is incorrect.\nex) 000-000-0000, 000-0000-0000"
+      );
+      join_inputs[INPUT_INDEX.TEL].focus();
+      return false;
+    }
+  };
+  document.querySelector("#login_btn").addEventListener("click", join);
+});
+
+const check_data = function () {
   const emailText = g_email.value;
   if (!emailText) {
     g_email.style.border = "1px solid #ff1212";
@@ -22,5 +85,5 @@ const eamilKeyUp = function () {
     g_email.style.border = "1px solid mediumseagreen";
   }
 
-  document.querySelector("#login_btn").addEventListener("click", join);
+  // document.querySelector("#login_btn").addEventListener("click", join);
 };
