f7e9ca528ddc1a1b346ef906954ed25ce6245da8
elquineas <elquineas@naver.com>
2023-06-23 13:20:18 +0900

    23-06-23_수정본

--- js/join.js
+++ js/join.js
@@ -16,6 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
       msg_divs[INPUT_INDEX.EMAIL].style.display = "block";
       msg_divs[INPUT_INDEX.EMAIL].textContent = nav_text;
       msg_divs[INPUT_INDEX.EMAIL].style.color = "tomato";
+      join_inputs[INPUT_INDEX.EMAIL].focus();
       return false;
     }
     const email_check =
@@ -25,52 +26,85 @@ document.addEventListener("DOMContentLoaded", () => {
       msg_divs[INPUT_INDEX.EMAIL].style.display = "block";
       msg_divs[INPUT_INDEX.EMAIL].textContent = nav_text;
       msg_divs[INPUT_INDEX.EMAIL].style.color = "tomato";
+      join_inputs[INPUT_INDEX.EMAIL].focus();
       return false;
     }
+    msg_divs[INPUT_INDEX.EMAIL].style.display = "none";
 
     const input_password = join_inputs[INPUT_INDEX.PASSWORD].value;
     if (!input_password) {
-      alert("Password is required.");
+      const nav_text = "* 비밀번호를 입력해주세요.";
+      msg_divs[INPUT_INDEX.PASSWORD].style.display = "block";
+      msg_divs[INPUT_INDEX.PASSWORD].textContent = nav_text;
+      msg_divs[INPUT_INDEX.PASSWORD].style.color = "tomato";
       join_inputs[INPUT_INDEX.PASSWORD].focus();
       return false;
     }
     const password_check =
       /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
     if (!password_check.test(input_password)) {
-      alert(
-        "Password must be 8 to 15 digits including \nspecial characters, uppercase and lowercase letters, and numbers."
-      );
+      const nav_text =
+        "* 비밀번호는 8~15자로 특수문자, 영문, 숫자를 입력해주세요.";
+      msg_divs[INPUT_INDEX.PASSWORD].style.display = "block";
+      msg_divs[INPUT_INDEX.PASSWORD].textContent = nav_text;
+      msg_divs[INPUT_INDEX.PASSWORD].style.color = "tomato";
       join_inputs[INPUT_INDEX.PASSWORD].focus();
       return false;
     }
+    msg_divs[INPUT_INDEX.PASSWORD].style.display = "none";
 
     const input_re_password = join_inputs[INPUT_INDEX.RE_PASSWORD].value;
     if (!input_re_password) {
-      alert("Password Check is required.");
+      const nav_text = "* 비밀번호 확인을 입력해주세요.";
+      msg_divs[INPUT_INDEX.RE_PASSWORD].style.display = "block";
+      msg_divs[INPUT_INDEX.RE_PASSWORD].textContent = nav_text;
+      msg_divs[INPUT_INDEX.RE_PASSWORD].style.color = "tomato";
       join_inputs[INPUT_INDEX.RE_PASSWORD].focus();
       return false;
     }
     if (input_password !== input_re_password) {
-      alert("Passwords do not match.");
+      const nav_text = "* 비밀번호와 동일하게 입력해주세요.";
+      msg_divs[INPUT_INDEX.RE_PASSWORD].style.display = "block";
+      msg_divs[INPUT_INDEX.RE_PASSWORD].textContent = nav_text;
+      msg_divs[INPUT_INDEX.RE_PASSWORD].style.color = "tomato";
       join_inputs[INPUT_INDEX.RE_PASSWORD].value = "";
       join_inputs[INPUT_INDEX.RE_PASSWORD].focus();
       return false;
     }
+    msg_divs[INPUT_INDEX.RE_PASSWORD].style.display = "none";
 
     const input_tel = join_inputs[INPUT_INDEX.TEL].value;
     if (!input_tel) {
-      alert("Phone is required.");
+      const nav_text = "* 전화번호를 입력해주세요.";
+      msg_divs[INPUT_INDEX.TEL].style.display = "block";
+      msg_divs[INPUT_INDEX.TEL].textContent = nav_text;
+      msg_divs[INPUT_INDEX.TEL].style.color = "tomato";
       join_inputs[INPUT_INDEX.TEL].focus();
       return false;
     }
     const tel_check = /^\d{3}-\d{3,4}-\d{4}$/;
     if (!tel_check.test(input_tel)) {
-      alert(
-        "The phone number format is incorrect.\nex) 000-000-0000, 000-0000-0000"
-      );
+      const nav_text =
+        "* 전화번호를 형식에 맞춰서 입력해주세요. ex)000-0000-0000";
+      msg_divs[INPUT_INDEX.TEL].style.display = "block";
+      msg_divs[INPUT_INDEX.TEL].textContent = nav_text;
+      msg_divs[INPUT_INDEX.TEL].style.color = "tomato";
       join_inputs[INPUT_INDEX.TEL].focus();
       return false;
     }
+    msg_divs[INPUT_INDEX.TEL].style.display = "none";
+
+    const input_birth = join_inputs[INPUT_INDEX.BIRTH].value;
+    const birth_check = /^\d{4}-\d{2}-\d{2}$/;
+    if (!birth_check.test(input_birth)) {
+      const nav_text = "* 전화번호를 형식에 맞춰서 입력해주세요. ex)1999-01-01";
+      msg_divs[INPUT_INDEX.BIRTH].style.display = "block";
+      msg_divs[INPUT_INDEX.BIRTH].textContent = nav_text;
+      msg_divs[INPUT_INDEX.BIRTH].style.color = "tomato";
+      join_inputs[INPUT_INDEX.BIRTH].focus();
+      return false;
+    }
+    msg_divs[INPUT_INDEX.BIRTH].style.display = "none";
   };
   document.querySelector("#login_btn").addEventListener("click", join);
 });
